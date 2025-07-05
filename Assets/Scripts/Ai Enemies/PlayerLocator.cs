9de3f4f39647380eb93637818c9f2a27ddcb26c9
1034948 <1034948@student.sae.edu.au>
2025-07-05 19:33:48 +1000

    Added enemy Ai

---
+++ Assets/Scripts/Ai Enemies/PlayerLocator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rolls the ball around using physics based on target direction
+/// </summary>
+public class PlayerLocator : MonoBehaviour
+{
+    [Header("Core Properties")]
+    [SerializeField] private Rigidbody sphere;
+
+    [Header("AI Functionality")]
+    [SerializeField] private bool isCurrentlyChasing;
+    [SerializeField] private GameObject chaseTarget;
+
+
+    private void FixedUpdate()
+    {
+            //AI chasing movement
+            //We need a target
+            //We want to move towards the target
+            if (sphere.velocity.magnitude < 2.5)
+            {
+                //Need to know the DIRECTION from ME (the AI) to the TARGET (the PLAYER)
+                //The Vector maths is
+                //target.position - me.position
+                sphere.AddForce(chaseTarget.transform.position - gameObject.transform.position);
+            }
+    }
+
+}
